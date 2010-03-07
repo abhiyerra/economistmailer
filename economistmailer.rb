6f84989ff570edf664c060a4f9deaa8ba3165d96
Abhi Yerra <ykabhinav@gmail.com>
2010-03-06 22:58:25 -0800

    Added login

--- economistmailer.rb
+++ economistmailer.rb
@@ -2,25 +2,40 @@ require "rubygems"
 require 'open-uri'
 require 'nokogiri'
 require 'net/smtp'
-require 'smtp-tls'
+require 'rest_client'
+#require 'smtp-tls'
 
-FromEmail = 'bot@traytwo.com'
-Password = 'BOT_EMAIL_PASSWORD'
+FromEmail = ''
+Password = ''
 
-ToEmail = 'YOUR_EMAIL'
+ToEmail = ''
+
+EconomistEmail = ''
+EconomistPassword = ''
 
 @articles = []
 
 def get_print_edition
-  source = "http://www.economist.com/printedition/"
+  url = 'http://www.economist.com/printedition/index.cfm'
+  f =  RestClient.post(url, 
+                       'source' => 'login_payBarrier', 
+                       'email_address' => EconomistEmail, 
+                       'pword' => EconomistPassword, 
+                       'save_password' => '', 
+                       'returnURL' => '/printedition/index.cfm?source=login_payBarrier', 
+                       'cms_object_id' => 'Y', 
+                       'paybarrier' => '1', 
+                       'logging_in' => 'Y')
 
-  doc = Nokogiri::HTML(open(source))
+  doc = Nokogiri::HTML(f)
   doc.search("div.style-2").each do |section|
     cur_section = (section/'h1').inner_html
 
     section.search('div.block').each do |article|
-      @articles << get_article((article/'h2/a')[0].attributes['href'], cur_section)
-      sleep 1
+      article_url = (article/'h2/a')[0].attributes['href']
+
+      @articles << get_article(article_url, cur_section)
+      puts article_url
     end
   end
 end
@@ -39,7 +54,7 @@ def get_article article_url, section
 
   article = article.inner_html
 
-msgstr = <<END_MESSAGE
+  msgstr = <<END_MESSAGE
 From: Economist Bot <#{FromEmail}>
 To: #{ToEmail}
 Subject: #{title} (#{section})
@@ -56,10 +71,12 @@ end
 def send_articles
   get_print_edition
 
-   Net::SMTP.start('smtp.gmail.com', 587, 'traytwo.com', FromEmail, Password, 'plain') do |smtp|
-     @articles.each do |article|
-       smtp.send_message(article, FromEmail, ToEmail)
-     end
+  smtp = Net::SMTP.new 'smtp.gmail.com', 587 
+  smtp.enable_starttls_auto
+  smtp.start('traytwo.com', FromEmail, Password, 'plain') do |smtp|
+    @articles.each do |article|
+      smtp.send_message(article, FromEmail, ToEmail)
+    end
   end
 end
 
