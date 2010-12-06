0320082f49c59ca0ed3f29f388df92d9438d1cfc
Abhi Yerra <ykabhinav@gmail.com>
2010-12-05 19:41:29 -0800

    Final code.

--- economistmailer.rb
+++ economistmailer.rb
@@ -1,83 +1,42 @@
 require "rubygems"
 require 'open-uri'
+require 'uri'
 require 'nokogiri'
-require 'net/smtp'
 require 'rest_client'
-#require 'smtp-tls'
-
-FromEmail = ''
-Password = ''
-
-ToEmail = ''
 
 EconomistEmail = ''
-EconomistPassword = ''
+EconomistPassword = ""
 
 @articles = []
 
 def get_print_edition
-  url = 'http://www.economist.com/printedition/index.cfm'
-  f =  RestClient.post(url, 
-                       'source' => 'login_payBarrier', 
-                       'email_address' => EconomistEmail, 
-                       'pword' => EconomistPassword, 
-                       'save_password' => '', 
-                       'returnURL' => '/printedition/index.cfm?source=login_payBarrier', 
-                       'cms_object_id' => 'Y', 
-                       'paybarrier' => '1', 
-                       'logging_in' => 'Y')
+    url = 'http://www.economist.com/printedition/'
+    f =  RestClient.post(url,
+                         'source'        => 'login_payBarrier',
+                         'email_address' => EconomistEmail,
+                         'pword'         => EconomistPassword,
+                         'save_password' => '',
+                         'returnURL'     => '/printedition/index.cfm?source=login_payBarrier',
+                         'cms_object_id' => 'Y',
+                         'paybarrier'    => '1',
+                         'logging_in'    => 'Y')
 
-  doc = Nokogiri::HTML(f)
-  doc.search("div.style-2").each do |section|
-    cur_section = (section/'h1').inner_html
+    doc = Nokogiri::HTML(f)
+    doc.search("div.style-2").each do |section|
+        cur_section = (section/'h1').inner_html
 
-    section.search('div.block').each do |article|
-      article_url = (article/'h2/a')[0].attributes['href']
+        puts "** #{cur_section}"
+        section.search('div.block').each do |article|
+            article_url = URI.join(url, (article/'h2/a')[0].attributes['href'].to_s).to_s
 
-      @articles << get_article(article_url, cur_section)
-      puts article_url
+            article_span = (article/'span').text.strip
+            article_title = (article/'h2/a').text
+
+            title = "*** TODO #{article_title.eql?('') ? '' : "#{article_span} : "}#{article_title}"
+            puts article_url
+            puts title
+        end
     end
-  end
 end
 
-def get_article article_url, section
-  doc = Nokogiri::HTML(open(article_url))
-
-  title = (doc/'//title').inner_html
-  title = title.gsub('| The Economist', '')
-
-  article = (doc/'div.col-left')
-  (article/"script").remove
-  (article/"noscript").remove
-  (article/"style").remove
-  (article/"#add-comment-container").remove
-
-  article = article.inner_html
-
-  msgstr = <<END_MESSAGE
-From: Economist Bot <#{FromEmail}>
-To: #{ToEmail}
-Subject: #{title} (#{section})
-Content-Type: text/html; charset="us-ascii"
-
-#{article}
-
-<a href="#{article_url}">Article</a>
-END_MESSAGE
-
-  msgstr
-end
-
-def send_articles
-  get_print_edition
-
-  smtp = Net::SMTP.new 'smtp.gmail.com', 587 
-  smtp.enable_starttls_auto
-  smtp.start('traytwo.com', FromEmail, Password, 'plain') do |smtp|
-    @articles.each do |article|
-      smtp.send_message(article, FromEmail, ToEmail)
-    end
-  end
-end
-
-send_articles
+get_print_edition
