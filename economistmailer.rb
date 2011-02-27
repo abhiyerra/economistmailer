186cab3cacb445724fbd34bdaaf12e41a491d071
Abhi Yerra <abhi@berkeley.edu>
2011-02-27 13:48:11 -0800

    Get the actual article and put it as html.

--- economistmailer.rb
+++ economistmailer.rb
@@ -4,39 +4,47 @@ require 'uri'
 require 'nokogiri'
 require 'rest_client'
 
-EconomistEmail = ''
-EconomistPassword = ""
-
 @articles = []
 
 def get_print_edition
-    url = 'http://www.economist.com/printedition/'
-    f =  RestClient.post(url,
-                         'source'        => 'login_payBarrier',
-                         'email_address' => EconomistEmail,
-                         'pword'         => EconomistPassword,
-                         'save_password' => '',
-                         'returnURL'     => '/printedition/index.cfm?source=login_payBarrier',
-                         'cms_object_id' => 'Y',
-                         'paybarrier'    => '1',
-                         'logging_in'    => 'Y')
+  url = 'http://www.economist.com/printedition/'
+  f =  RestClient.get(url)
 
-    doc = Nokogiri::HTML(f)
-    doc.search("div.style-2").each do |section|
-        cur_section = (section/'h1').inner_html
+  puts "<html><head><title>Economist</title></head>"
 
-        puts "** #{cur_section}"
-        section.search('div.block').each do |article|
-            article_url = URI.join(url, (article/'h2/a')[0].attributes['href'].to_s).to_s
+  doc = Nokogiri::HTML(f)
+  doc.search("div.style-2").each do |section|
+    cur_section = (section/'h1').inner_html
 
-            article_span = (article/'span').text.strip
-            article_title = (article/'h2/a').text
+    puts "<h1>#{cur_section}</h1>"
 
-            title = "*** TODO #{article_title.eql?('') ? '' : "#{article_span} : "}#{article_title}"
-            puts article_url
-            puts title
-        end
+    section.search('div.block').each do |article|
+      article_url = URI.join(url, (article/'h2/a')[0].attributes['href'].to_s).to_s
+
+      article_span = (article/'span').text.strip
+      article_title = (article/'h2/a').text
+
+      get_article(article_url)
+      sleep 1
     end
+  end
+
+  puts "</html>"
+end
+
+def get_article(url)
+  f = RestClient.get(url)
+  doc = Nokogiri::HTML(f)
+
+  doc.search("#ec-article-body").each do |section|
+    %w{.share-links-header .related-items}.each do |class_name|
+      section.search(class_name).each do |node|
+        node.remove
+      end
+    end
+
+    puts section.inner_html.strip
+  end
 end
 
 get_print_edition
